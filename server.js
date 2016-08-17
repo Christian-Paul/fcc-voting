22e6d27e99df40db7e95785e20d7da03992b4b5c
Christian Paul <christiansamuelpaul@gmail.com>
2016-08-17 14:27:23 -0400

    Add create new poll functionality

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://chris:password@ds161245.mlab.com:61245/fcc-voting');
 
+var pollSchema = new mongoose.Schema({
+	title: String,
+	author: String,
+	options: Array
+});
+
+var Poll = mongoose.model('Poll', pollSchema)
 
 app.listen(3000, function(req, res) {
 	console.log('listening on 3000');
@@ -15,4 +22,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/new-poll', function(req, res) {
+	var formData = req.body;
+	var optionsArr = formData.options.split('\r\n').map(function(item) {
+		return {
+			option: item,
+			votes: 0
+		}
+	});
+	var author = formData.author || 'Anonymous';
+
+	var newPoll = new Poll({
+		title: formData.title,
+		author: author,
+		options: optionsArr
+	});
+
+	newPoll.save(function(err) {
+		if(err) {
+			console.log(err);
+		}
+	});
+
+	res.redirect('/')
 });
