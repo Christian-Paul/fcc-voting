1bf85f567ccadbae1a34cfeb1d8b4188cd425004
Christian Paul <christiansamuelpaul@gmail.com>
2016-08-23 06:18:18 -0400

    More style improvements. User menu can now be closed by clicking elsewhere on page. Fonts thinner, added logo

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,5 +1,14 @@
-var menu = document.querySelector('.user-interface');
+var menu = $('.user-interface');
 
-menu.onclick = function() {
-	menu.classList.toggle('triggered');
-};
+// toggle menu when clicking on the menu icon
+menu.on('click', function() {
+	menu.toggleClass('triggered');
+});
+
+// when the document is clicked, check if the target is user-interface or a descendent
+// if the target is not the UI, close the ui
+$(document).on('click', function(event) {
+	if (!$(event.target).closest(menu).length) {
+		menu.removeClass('triggered');
+	}
+});
